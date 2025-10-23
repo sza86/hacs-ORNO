b1f52a3015710189a06378d56c2d055308f35ed3
sza86 <44470343+sza86@users.noreply.github.com>
2025-10-23 06:29:47 +0200

    Delete custom_components directory

--- custom_components/orno_517/const.py
+++
@@ -1,9 +0,0 @@
-"""Stałe dla integracji ORNO 517."""
-DOMAIN = "orno_517"
-CONF_HOST = "host"
-CONF_PORT = "port"
-CONF_SLAVE = "slave"
-CONF_SCAN_INTERVAL = "scan_interval"
-CONF_DEVICE_NAME = "device_name"
-
-PLATFORMS = ["sensor"]
